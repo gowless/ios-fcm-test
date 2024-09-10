ca68d9404aff98cdc0a541bbecfc37488920e50d
Denys Denysenko <farenotwells@gmail.com>
2024-09-10 17:57:58 +0300

    Initial Commit

---
+++ FCM-Test/FCM_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FCM_TestApp.swift
+//  FCM-Test
+//
+//  Created by Denys on 10.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct FCM_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
